7ec0ab855cec2a62c73843ebc037c4169e4b6f48
vishvanand <svishvanand@gmail.com>
2018-12-15 21:32:09 -0800

    Refactor + Add unit tests

---
+++ server/db/init-db.py
@@ -0,0 +1,21 @@
+from pymongo import MongoClient
+from collections import OrderedDict
+import sys
+import schema
+
+
+# Setup real db.
+# This will fail if the db already exists.
+client = MongoClient() 
+db = client.prod
+
+# WARNING: ONLY RUN THE FOLLOWING COMMAND IF YOU WANT TO NUKE THE DB.
+# db.cono_tag_entity_db.drop()
+
+db.create_collection("cono_tag_entity_db")
+
+query = [('collMod', 'cono_tag_entity_db'),
+        ('validator', schema.tag_entity_validator),
+        ('validationLevel', 'moderate')]
+query = OrderedDict(query)
+db.command(query)
--- server/db/init-db_test.py
+++ server/db/init-db_test.py
@@ -3,12 +3,14 @@ from collections import OrderedDict
 import sys
 import schema
 
+
+# Setup test follection.
 client = MongoClient() 
 db = client.testX
 
 db.cono_tag_entity_testdb.drop()
 
-db.create_collection("cono_tag_entity_testdb")  # Force create!
+db.create_collection("cono_tag_entity_testdb")
 
 query = [('collMod', 'cono_tag_entity_testdb'),
         ('validator', schema.tag_entity_validator),
