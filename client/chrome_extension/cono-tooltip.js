da4447327d50dd34db5219be32ac4ba0a5e831f7
Max Woo <maxithewoowoo@gmail.com>
2018-12-22 21:14:01 -0800

    converted content script code to use jquery

--- client/chrome_extension/cono-tooltip.js
+++ client/chrome_extension/cono-tooltip.js
@@ -1,14 +1,11 @@
 // initializes and adds click listeners to a tooltip
 function initTooltip(element) {
-	element.querySelector('.cono-add').addEventListener("click", (e) => {
-		e.target.classList.toggle('cono-adding');
-		element.querySelector('.cono-add-dialogue').classList.toggle('cono-adding');
+	element.find('.cono-add').click(e => {
+		$(e.target).toggleClass('cono-adding');
+		element.find('.cono-add-dialogue').toggleClass('cono-adding');
 	});
 
-	var tagButtons = element.querySelectorAll('.cono-tag');
-	for (var i = 0; i < tagButtons.length; i++) {
-		tagButtons[i].addEventListener("click", (e) => {
-			e.target.classList.toggle("cono-personal");
-		});
-	}
+	var tagButtons = element.find('.cono-tag').click((e) => {
+		$(e.target).toggleClass("cono-personal");
+	});
 }
