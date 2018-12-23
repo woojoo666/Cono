77edc0adaa5dc448c8227deb6f42a94262be7738
Max Woo <maxithewoowoo@gmail.com>
2018-12-22 19:13:08 -0800

    spiced up the popup main page
    added a big logo, click to enable/disable

--- client/chrome_extension/dialog/main.js
+++ client/chrome_extension/dialog/main.js
@@ -1,14 +1,14 @@
 pages.main.init = function () {
-	$('button.sign-out').click((e) => {
+	$('.sign-out').click((e) => {
 		sendMessage({ action: 'sign_out' });
 	});
 	$('#username').text(username);
-	var toggleButton = $('button.toggle-tooltips');
+	var toggleButton = $('.toggle-tooltips');
 
 	function updateToggleButton() {
 		sendMessage({action: 'get-tooltips-enabled'}, response => {
-			toggleButton.toggleClass('tooltips-enabled', response.tooltips_enabled); // enable class based on response
-			toggleButton.text( response.tooltips_enabled ? 'Tag Tooltips Enabled' : 'Tag Tooltips Disabled' );
+			toggleButton.toggleClass('enabled', response.tooltips_enabled); // enable class based on response
+			$('.caption').text( response.tooltips_enabled ? 'Tag Tooltips Enabled' : 'Tag Tooltips Disabled' );
 		});
 	}
 
